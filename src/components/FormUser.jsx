dc4402262f47a95f5ce07aacfb9e692a816c3fbc
anthony sanchez <anthonysa0813@gmail.com>
2021-07-31 08:40:38 -0500

    trabajando en el login y registro con hooks

--- src/components/FormUser.jsx
+++ src/components/FormUser.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { db, auth } from "../firebase";
+import "../App.css";
 
 const FormUser = () => {
+  const [login, setLogin] = useState(false);
   const [user, setUser] = useState({
     email: "",
     password: "",
@@ -25,23 +27,86 @@ const FormUser = () => {
 
   return (
     <form onSubmit={registro}>
-      <h1>Registrarse</h1>
+      <h1>{login ? "Ingresa tu cuenta" : "Registro"}</h1>
 
-      <input
-        type="text"
-        className="email"
-        placeholder="Escribe tu email"
-        name="email"
-        onChange={(e) => setUser({ ...user, [e.target.name]: e.target.value })}
-      />
-      <input
-        type="text"
-        className="password"
-        placeholder="Escribe tu password"
-        name="password"
-        onChange={(e) => setUser({ ...user, [e.target.name]: e.target.value })}
-      />
-      <button type="submit">Registrarme</button>
+      {login ? (
+        <>
+          <input
+            type="text"
+            className="email"
+            placeholder="Escribe tu email"
+            name="email"
+            onChange={(e) =>
+              setUser({ ...user, [e.target.name]: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            className="password"
+            placeholder="Escribe tu password"
+            name="password"
+            onChange={(e) =>
+              setUser({ ...user, [e.target.name]: e.target.value })
+            }
+          />
+        </>
+      ) : (
+        <>
+          <input
+            type="text"
+            className="email"
+            placeholder="Escribe tu email"
+            name="email"
+            onChange={(e) =>
+              setUser({ ...user, [e.target.name]: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            className="names"
+            placeholder="Escribe tu Nombre completo"
+            name="names"
+            onChange={(e) =>
+              setUser({ ...user, [e.target.name]: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            className="password"
+            placeholder="Escribe tu email"
+            name="password"
+            onChange={(e) =>
+              setUser({ ...user, [e.target.name]: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            className="password"
+            placeholder="Escribe tu código postal"
+            name="password"
+            onChange={(e) =>
+              setUser({ ...user, [e.target.name]: e.target.value })
+            }
+          />
+        </>
+      )}
+      {login ? (
+        <button
+          type="submit"
+          className={login ? "login" : "registrar"}
+          onClick={() => setLogin(!login)}
+        >
+          Entrar
+        </button>
+      ) : (
+        <button
+          type="submit"
+          className={login ? "login" : "registrar"}
+          onClick={() => setLogin(!login)}
+        >
+          Crear Cuenta
+        </button>
+      )}
     </form>
   );
 };
